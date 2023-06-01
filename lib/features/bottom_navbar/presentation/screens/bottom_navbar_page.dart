f150313a419146c3720369f957883fa441d9aeab
Thaanu <thaanup@gmail.com>
2023-06-01 14:18:55 +0530

    Initial commit

---
+++ lib/features/bottom_navbar/presentation/screens/bottom_navbar_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'bottom_navbar_view.dart';
+
+class BottomNavbarPage extends StatelessWidget {
+  const BottomNavbarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const BottomNavbarView();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'features/bottom_navbar/presentation/screens/bottom_navbar_page.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'News App Orel',
+      home: BottomNavbarPage(),
+    );
+  }
+}
